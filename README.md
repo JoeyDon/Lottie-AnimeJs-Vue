b137f3286a70f8c7f21e48bc59e08a56452cc298
Joey <joey.don0905@gmail.com>
2019-07-24 23:47:08 +1000

    first commit

--- README.md
+++ README.md
@@ -1,29 +1,42 @@
 # lottie-demo
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Run your tests
+
 ```
 npm run test
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+#Roadmap
+
+1. <lottie-player/> with <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+
+2. Can't manipulate size, so install module lottie-web
+   rendererSettings: { viewBoxSize: "700 0 2400 590" }
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div ref="lottieContainer"></div>
+
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import animation from "./instruction/animation.json";
+import lottie from "lottie-web";
+//import * as bodymovin from "./lottie_light.min.js"
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  mounted() {
+    this.anim = lottie.loadAnimation({
+      container: this.$refs.lottieContainer,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: animation,
+      rendererSettings: { viewBoxSize: "400 400 800 800" }
+    });
   }
-}
+};
 </script>
 
 <style>
+#lottiePlayer {
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
 </style>
