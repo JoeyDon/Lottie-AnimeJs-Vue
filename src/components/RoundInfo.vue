993370ccdeb4a45ad34119b4d6b2d50fd7ad44a6
Joey <joey.don0905@gmail.com>
2019-07-26 20:40:36 +1000

    GRID ADDED

--- src/components/RoundInfo.vue
+++ src/components/RoundInfo.vue
@@ -1,39 +1,13 @@
 <template>
-  <div id="page-wrap">
-    <h1>Progress Bars</h1>
-	  
-    <p>
-      They should stretch fine to whatever the width of the container element is, 
-      or just set it. Also they should stretch fine to whatever height you give .meter.
-    </p>
-
-    <div class="meter">
-      <span style="width: 25%" />
+  <div>
+    <div class="grid-container">
+      <div class="item1">YOUR FIRST WALK</div>
+      <div class="item2">
+        <img class="icon" src="../assets/icon@2x.png" />
+      </div>
+      <div class="item3">Main</div>
+      <div class="item4">Footer</div>
     </div>
-    <pre><code>&lt;div class=&quot;meter&quot;&gt;
-	&lt;span style=&quot;width: 25%&quot;&gt;&lt;/span&gt;
-&lt;/div&gt;</code></pre>
-		
-    <div class="meter orange nostripes">
-      <span style="width: 33.3%" />
-    </div>
-    <pre><code>&lt;div class=&quot;meter orange nostripes&quot;&gt;
-	&lt;span style=&quot;width: 33.3%&quot;&gt;&lt;/span&gt;
-&lt;/div&gt;</code></pre>		
-		
-    <div class="meter red">
-      <span style="width: 80%" />
-    </div>
-    <pre><code>&lt;div class=&quot;meter red&quot;&gt;
-	&lt;span style=&quot;width: 80%&quot;&gt;&lt;/span&gt;
-&lt;/div&gt;</code></pre>	
-
-    <div class="meter animate">
-      <span style="width: 50%"><span /></span>
-    </div>
-    <pre><code>&lt;div class=&quot;meter animate&quot;&gt;
-	&lt;span style=&quot;width: 50%&quot;&gt;&lt;span&gt;&lt;/span&gt;&lt;/span&gt;
-&lt;/div&gt;</code></pre>
   </div>
 </template>
 
@@ -44,7 +18,7 @@ export default {
   props: {
     data: Object
   },
-  mounted(){
+  mounted() {
     // anime({
     //   targets: '.line',
     //   width: '100%', // -> from '28px' to '100%',
@@ -52,145 +26,43 @@ export default {
     //   direction: 'alternate',
     //   loop:true
     // });
-    $(function() {
-			$(".meter > span").each(function() {
-				$(this)
-					.data("origWidth", $(this).width())
-					.width(0)
-					.animate({
-						width: $(this).data("origWidth")
-					}, 1200);
-			});
-		});
   }
 };
 </script>
 
 <style>
-		.meter { 
-			height: 20px;  /* Can be anything */
-			position: relative;
-			margin: 60px 0 20px 0; /* Just for demo spacing */
-			background: #555;
-			-moz-border-radius: 25px;
-			-webkit-border-radius: 25px;
-			border-radius: 25px;
-			padding: 10px;
-			-webkit-box-shadow: inset 0 -1px 1px rgba(255,255,255,0.3);
-			-moz-box-shadow   : inset 0 -1px 1px rgba(255,255,255,0.3);
-			box-shadow        : inset 0 -1px 1px rgba(255,255,255,0.3);
-		}
-		.meter > span {
-			display: block;
-			height: 100%;
-			   -webkit-border-top-right-radius: 8px;
-			-webkit-border-bottom-right-radius: 8px;
-			       -moz-border-radius-topright: 8px;
-			    -moz-border-radius-bottomright: 8px;
-			           border-top-right-radius: 8px;
-			        border-bottom-right-radius: 8px;
-			    -webkit-border-top-left-radius: 20px;
-			 -webkit-border-bottom-left-radius: 20px;
-			        -moz-border-radius-topleft: 20px;
-			     -moz-border-radius-bottomleft: 20px;
-			            border-top-left-radius: 20px;
-			         border-bottom-left-radius: 20px;
-			background-color: rgb(43,194,83);
-			background-image: -webkit-gradient(
-			  linear,
-			  left bottom,
-			  left top,
-			  color-stop(0, rgb(43,194,83)),
-			  color-stop(1, rgb(84,240,84))
-			 );
-			background-image: -moz-linear-gradient(
-			  center bottom,
-			  rgb(43,194,83) 37%,
-			  rgb(84,240,84) 69%
-			 );
-			-webkit-box-shadow: 
-			  inset 0 2px 9px  rgba(255,255,255,0.3),
-			  inset 0 -2px 6px rgba(0,0,0,0.4);
-			-moz-box-shadow: 
-			  inset 0 2px 9px  rgba(255,255,255,0.3),
-			  inset 0 -2px 6px rgba(0,0,0,0.4);
-			box-shadow: 
-			  inset 0 2px 9px  rgba(255,255,255,0.3),
-			  inset 0 -2px 6px rgba(0,0,0,0.4);
-			position: relative;
-			overflow: hidden;
-		}
-		.meter > span:after, .animate > span > span {
-			content: "";
-			position: absolute;
-			top: 0; left: 0; bottom: 0; right: 0;
-			background-image: 
-			   -webkit-gradient(linear, 0 0, 100% 100%, 
-			      color-stop(.25, rgba(255, 255, 255, .2)), 
-			      color-stop(.25, transparent), color-stop(.5, transparent), 
-			      color-stop(.5, rgba(255, 255, 255, .2)), 
-			      color-stop(.75, rgba(255, 255, 255, .2)), 
-			      color-stop(.75, transparent), to(transparent)
-			   );
-			background-image: 
-				-moz-linear-gradient(
-				  -45deg, 
-			      rgba(255, 255, 255, .2) 25%, 
-			      transparent 25%, 
-			      transparent 50%, 
-			      rgba(255, 255, 255, .2) 50%, 
-			      rgba(255, 255, 255, .2) 75%, 
-			      transparent 75%, 
-			      transparent
-			   );
-			z-index: 1;
-			-webkit-background-size: 50px 50px;
-			-moz-background-size: 50px 50px;
-			-webkit-animation: move 2s linear infinite;
-			   -webkit-border-top-right-radius: 8px;
-			-webkit-border-bottom-right-radius: 8px;
-			       -moz-border-radius-topright: 8px;
-			    -moz-border-radius-bottomright: 8px;
-			           border-top-right-radius: 8px;
-			        border-bottom-right-radius: 8px;
-			    -webkit-border-top-left-radius: 20px;
-			 -webkit-border-bottom-left-radius: 20px;
-			        -moz-border-radius-topleft: 20px;
-			     -moz-border-radius-bottomleft: 20px;
-			            border-top-left-radius: 20px;
-			         border-bottom-left-radius: 20px;
-			overflow: hidden;
-		}
-		
-		.animate > span:after {
-			display: none;
-		}
-		
-		@-webkit-keyframes move {
-		    0% {
-		       background-position: 0 0;
-		    }
-		    100% {
-		       background-position: 50px 50px;
-		    }
-		}
-		
-		.orange > span {
-			background-color: #f1a165;
-			background-image: -moz-linear-gradient(top, #f1a165, #f36d0a);
-			background-image: -webkit-gradient(linear,left top,left bottom,color-stop(0, #f1a165),color-stop(1, #f36d0a));
-			background-image: -webkit-linear-gradient(#f1a165, #f36d0a); 
-		}
-		
-		.red > span {
-			background-color: #f0a3a3;
-			background-image: -moz-linear-gradient(top, #f0a3a3, #f42323);
-			background-image: -webkit-gradient(linear,left top,left bottom,color-stop(0, #f0a3a3),color-stop(1, #f42323));
-			background-image: -webkit-linear-gradient(#f0a3a3, #f42323);
-		}
-		
-		.nostripes > span > span, .nostripes > span:after {
-			-webkit-animation: none;
-			background-image: none;
-		}
-	</style>
+.item1 {
+  grid-area: header;
+}
+.item2 {
+  grid-area: icon;
+}
+.item3 {
+  grid-area: main;
+}
+.item4 {
+  grid-area: footer;
+}
+
+.grid-container {
+  grid-template-columns: 50px;
+  border-radius: 44px;
+  display: grid;
+  grid-template-areas:
+    "icon header header header header header"
+    "icon main main main main main"
+    "icon footer footer footer footer footer";
+  grid-gap: 2px;
+  background-color: #2196f3;
+  padding: 10px;
+}
+
+.grid-container > div {
+  background-color: rgba(255, 255, 255, 0.8);
+  font-size: 10px;
+}
+
+.grid-container > .item4 {
+  text-align: right;
+}
+</style>
